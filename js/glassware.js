aaa243915e8c6e2ed4c33799cd9d675cc0070b8c
Wonshtrum <pygmee64@gmail.com>
2020-03-22 23:47:46 +0100

    Refactor

--- js/glassware.js
+++ js/glassware.js
@@ -1,61 +1,85 @@
 class Tile {
-	constructor(x, y, size) {
-		this.x = x;
-		this.y = y;
+	constructor(x, y, size, shelf) {
+		this.x = x-colH;
+		this.y = y-rowH;
 		this.size = size;
+		this.shelf = shelf;
+		Tile.list.push(this);
+		for (let i = 0 ; i < size ; i++) {
+			for (let j = 0 ; j < size ; j++) {
+				Tile.mat[y+j][x+i] = this;
+			}
+		}
+	}
+	draw(ctx) {
+		if (this.shelf)
+			ctx.drawQuad(this.x*side, this.y*side, this.size*side, this.size*side, 3, 0, 0, 0, 0);
 	}
 }
+Tile.list = [];
+Tile.mat = Array.from({length: row}, () => Array(col));
 
 class Flask extends Tile {
 	constructor(x, y, size, level, R, G, B) {
-		super(x, y, size);
-		this.level = level;
-		this.fill = fill;
+		super(x, y, size, false);
+		this.level = 0;
+		this.fill(level);
 		this.R = R;
 		this.G = G;
 		this.B = B;
 	}
 	fill(level) {
-		this.fill = Math.max(level, 0.999);
+		this.level = Math.min(level, 0.999);
 	}
 }
 
 class Erlenmeyer extends Flask {
 	draw(ctx) {
-		ctx.drawQuad(this.x*side, this.y*side, this.size*side, this.size*side, 0, this.fill, this.R, this.G, this.B);
+		super.draw(ctx);
+		ctx.drawQuad(this.x*side, this.y*side, this.size*side, this.size*side, 0, this.level, this.R, this.G, this.B);
 	}
 }
 
 class Bescher extends Flask {
 	draw(ctx) {
-		ctx.drawQuad(this.x*side, this.y*side, this.size*side, this.size*side, 1, this.fill, this.R, this.G, this.B);
+		super.draw(ctx);
+		ctx.drawQuad(this.x*side, this.y*side, this.size*side, this.size*side, 1, this.level, this.R, this.G, this.B);
 	}
 }
 
 class Distillation extends Flask {
 	draw(ctx) {
-		ctx.drawQuad(this.x*side, this.y*side, this.size*side, this.size*side, 2, this.fill, this.R, this.G, this.B);
+		super.draw(ctx);
+		ctx.drawQuad(this.x*side, this.y*side, this.size*side, this.size*side, 2, this.level, this.R, this.G, this.B);
 	}
 }
 
 class Shelf extends Tile {
-	draw(ctx) {
-		ctx.drawQuad(this.x*side, this.y*side, this.size*side, this.size*side, 3, 0, 0, 0, 0);
+	constructor(x, y, size) {
+		super(x, y, size, true);
 	}
 }
 
 class Spout extends Tile {
 	constructor(x, y, size, lit) {
-		super(x, y, size);
-		this.lit = lit;
+		super(x, y, size, false);
+		this.level = 0;
+		this.lit(lit);
+	}
+	lit(lit) {
+		this.level = lit ? 0.999 : 0;
 	}
 	draw(ctx) {
-		ctx.drawQuad(this.x*side, this.y*side, this.size*side, this.size*side, 0, this.fill, this.R, this.G, this.B);
+		super.draw(ctx);
+		ctx.drawQuad(this.x*side, this.y*side, this.size*side, this.size*side, 4, this.level, 0, 0, 0);
 	}
 }
 
+Tile.types = [Erlenmeyer, Bescher, Distillation, Shelf, Spout];
+
 class Pipe {
 	constructor(x, y, path) {
+		Pipe.list.push(this);
 		this.path = [];
 		let X = colH*5;
 		let Y = rowH*5;
@@ -82,6 +106,49 @@ class Pipe {
 	}
 	draw(ctx) {
 		for (let [x, y, t] of this.path)
-			ctx.drawQuad(this.x*pside, this.y*pside, pside, pside, t, 0, 0, 0, 0);
+			ctx.drawQuad(this.x, this.y, pside, pside, t, 0, 0, 0, 0);
 	}
+}
+Pipe.list = [];
+
+let obj = [
+	[0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,1,0,3,0,3,0,0],
+	[0,0,2,0,0,3,0,3,0,0],
+	[0,0,0,0,2,3,0,3,0,0],
+	[0,0,0,0,0,5,0,0,0,0]];
+let shelf = [
+	[0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,1,0,1,0,0],
+	[0,0,0,1,0,1,0,1,0,0],
+	[0,0,1,1,0,1,0,1,0,0],
+	[0,0,1,1,1,1,0,1,0,0]];
+let fill = [
+	[0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,.4,0,.7,0,0,0,0],
+	[0,0,.2,0,0,1,0,0,0,0],
+	[0,0,0,0,.1,1,0,0,0,0],
+	[0,0,0,0,0,.9,0,0,0,0]];
+let pipes = [
+	[[3,1,2,4,0,30],[0,-5],[4,0],[0,15]],
+	[[5,1,2,2,0,43],[0,-5],[-4,0],[0,15]],
+	[[5,3,2,4,0,7],[5,0],[0,1],[-1,0],[0,-2],[2,0],[0,-5],[-1,0],[0,4],[2,0],[0,-1],[-7,0]],
+	[[2,2,2,5,0,12],[0,-6],[-4,0],[0,-1],[1,0],[0,2],[-5,0],[0,1],[3,0],[0,1],[-2,0],[0,1],[1,0],[0,6]]
+];
+
+let tmp;
+for (let i = 0 ; i < 10 ; i++) {
+	for (let j = 0 ; j < 5 ; j++) {
+		if (obj[j][i]) {
+			tmp = new Tile.types[obj[j][i]-1](i, j, 1, fill[j][i], 0, 0.8, 0.75);
+			if (shelf[j][i])
+				tmp.shelf = true;
+		} else if (shelf[j][i])
+			new Shelf(i, j, 1);
+	}
+}
+
+for (let pipe of pipes) {
+	let [X, Y, x, y] = pipe[0];
+	new Pipe(X*5+x, Y*5+y, pipe.splice(0));
 }
--- js/renderer.js
+++ js/renderer.js
@@ -15,90 +15,16 @@ let b1 = new BatchVA(2000, ()=>{
 
 unbindAllFbo = FrameBuffer.prototype.unbind;
 
-let obj = [
-	[0,0,0,0,0,0,0,0,0,0],
-	[0,0,0,1,0,3,0,3,0,0],
-	[0,0,2,0,0,3,0,3,0,0],
-	[0,0,0,0,2,3,0,3,0,0],
-	[0,0,0,0,0,5,0,0,0,0]]
-let shaft = [
-	[0,0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,1,0,1,0,0],
-	[0,0,0,1,0,1,0,1,0,0],
-	[0,0,1,1,0,1,0,1,0,0],
-	[0,0,1,1,1,1,0,1,0,0]]
-let fill = [
-	[0,0,0,0,0,0,0,0,0,0],
-	[0,0,0,.4,0,.7,0,0,0,0],
-	[0,0,.2,0,0,.99,0,0,0,0],
-	[0,0,0,0,.1,.99,0,0,0,0],
-	[0,0,0,0,0,.9,0,0,0,0]]
-R=0;
-G=.8;
-B=.75;
-let pipes = [
-	[[3,1,2,4,0,30],[0,-5],[4,0],[0,15]],
-	[[5,1,2,2,0,43],[0,-5],[-4,0],[0,15]],
-	[[5,3,2,4,0,7],[5,0],[0,1],[-1,0],[0,-2],[2,0],[0,-5],[-1,0],[0,4],[2,0],[0,-1],[-7,0]],
-	[[2,2,2,5,0,12],[0,-6],[-4,0],[0,-1],[1,0],[0,2],[-5,0],[0,1],[3,0],[0,1],[-2,0],[0,1],[1,0],[0,6]]
-];
-const drawPipe = (pipe) => {
-	let [x,y,xx,yy,fb,fe] = pipe[0];
-	x = 5*x+xx;
-	y = 5*y+yy;
-	let dx, dy, ddx, ddy, f;
-	f = 0;
-	let fill = () => {f++; return f > fb && f <= fb+fe ? .99 : 0;}
-	ddx = ddy = 0;
-	for (let i = 1 ; i < pipe.length ; i++) {
-		[dx,dy] = pipe[i];
-		if (dx) {
-			ddx = dx > 0 ? 1 : -1;
-			if (i != 1)
-				b1.drawQuad((x-25)*pside,(y-12.5)*pside,pside,pside,9-ddx+3*ddy,fill(),R,G,B);
-			for (let j = ddx ; j != dx ; j+=ddx)
-				b1.drawQuad((x+j-25)*pside,(y-12.5)*pside,pside,pside,6,fill(),R,G,B);
-		} else {
-			ddy = dy > 0 ? 1 : -1;
-			if (i != 1)
-				b1.drawQuad((x-25)*pside,(y-12.5)*pside,pside,pside,9+ddx-3*ddy,fill(),R,G,B);
-			for (let j = ddy ; j != dy ; j+=ddy)
-				b1.drawQuad((x-25)*pside,(y+j-12.5)*pside,pside,pside,8,fill(),R,G,B);
-		}
-		x += dx;
-		y += dy;
-	}
-}
-dR = 0.01;
-dG = 0.025;
-dB = 0.03;
+R = 0;
+G = 0.8;
+B = 0.75;
 const render = () => {
-	R += dR;
-	G += dG;
-	B += dB;
-	if (R>1 || R<0) dR*=-1;
-	if (G>1 || G<0) dG*=-1;
-	if (B>1 || B<0) dB*=-1;
 	b1.bind();
 	b1.begin();
-	for (let i = 0 ; i < 10 ; i++) {
-		for (let j = 0 ; j < 5 ; j++) {
-			if (shaft[j][i])
-				b1.drawQuad((i-5)*side,(j-2.5)*side,side,side,3,0,0,0,0);
-		}
-	}
-	for (let pipe of pipes) {
-		drawPipe(pipe);
-		if (pipe[0][4]++>40) pipe[0][4]=-pipe[0][5];
-	}
-	for (let i = 0 ; i < 10 ; i++) {
-		for (let j = 0 ; j < 5 ; j++) {
-			if (obj[j][i] == 5)
-				b1.drawQuad((i-5)*side,(j-2.5)*side,side,side,obj[j][i]-1,fill[j][i],0,0,0);
-			else if (obj[j][i])
-				b1.drawQuad((i-5)*side,(j-2.5)*side,side,side,obj[j][i]-1,fill[j][i],R,G,B);
-		}
-	}
+	for (let pipe of Pipe.list)
+		pipe.draw(b1);
+	for (let tile of Tile.list)
+		tile.draw(b1);
 	b1.drawQuad(-5*side,.5*side,side*2,side*2,1,0,R,G,B);
 	b1.drawQuad(-3*side,.5*side,side*2,side*2,0,.4,R,G,B);
 	b1.drawQuad(2*side,.5*side,side*2,side*2,0,0,R,G,B);
